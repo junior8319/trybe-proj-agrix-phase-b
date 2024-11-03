a0d321885706c08de592df8ea46af39a3304e954
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2024-11-03 19:23:34 -0300

    feat-req 8-set the relationship between entities for Fertilizers and Crops.

--- src/main/java/com/betrybe/agrix/entity/Crop.java
+++ src/main/java/com/betrybe/agrix/entity/Crop.java
@@ -5,9 +5,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.time.LocalDate;
+import java.util.List;
 
 /**
  * The type Farm.
@@ -30,6 +32,9 @@ public class Crop {
   @ManyToOne
   @JoinColumn(name = "farm_id")
   private Farm farm;
+
+  @ManyToMany(mappedBy = "crops")
+  private List<Fertilizer> fertilizers;
 
   /**
    * Instantiates a new Crop.
@@ -153,5 +158,23 @@ public class Crop {
    */
   public void setHarvestDate(LocalDate harvestDate) {
     this.harvestDate = harvestDate;
+  }
+
+  /**
+   * Gets fertilizers.
+   *
+   * @return the fertilizers
+   */
+  public List<Fertilizer> getFertilizers() {
+    return fertilizers;
+  }
+
+  /**
+   * Sets fertilizers.
+   *
+   * @param fertilizers the fertilizers
+   */
+  public void setFertilizers(List<Fertilizer> fertilizers) {
+    this.fertilizers = fertilizers;
   }
 }
